0ad2c1c0c7cca0c5532c5d8f68981eb224726633
somogyiandrasistvan <somogyiandraska@gmail.com>
2023-11-20 15:20:47 +0100

    harmadik feladat

--- src/main/KocsimosoProgram.java
+++ src/main/KocsimosoProgram.java
@@ -16,12 +16,16 @@ public class KocsimosoProgram {
 
         kocsimosoda = new Kocsimoso(4);
         kocsimosoda.bemegy(new Motor("342-WWW", "BMW"));
-        kocsimosoda.bemegy(new Motor("432-WFG", "Ford"));
         System.out.println(kocsimosoda.getJarmuvek());
+        System.out.println("-- mosas:");
         kocsimosoda.normalMosas();
         System.out.println(kocsimosoda.getJarmuvek());
-
+        System.out.println("-- kimegy:");
         kocsimosoda.kimegy();
+        System.out.println("-- tonkrement:");
+        kocsimosoda.tonkrement();
+        kocsimosoda.bemegy(new Motor("432-WFG", "Ford"));
+
         System.out.println(kocsimosoda.getJarmuvek());
 
     }
--- src/modell/KocsiMoso.java
+++ src/modell/KocsiMoso.java
@@ -4,30 +4,48 @@ public class Kocsimoso {
 
     private Jarmu[] jarmu;
     private boolean szabad;
+    private boolean tonkrement;
 
     public Kocsimoso(int db) {
         jarmu = new Jarmu[db];
         szabad = true;
+        tonkrement = false;
+    }
+
+    public void tonkrement() {
+        tonkrement = true;
     }
 
     public void bemegy(Jarmu kocsi) {
-        if (szabad == true) {
-            jarmu[0] = kocsi;
-            szabad = false;
-            System.out.println("Bekerült egy auto!");
+        if (tonkrement == false) {
+            if (szabad == true) {
+                jarmu[0] = kocsi;
+                szabad = false;
+                System.out.println("Bekerült egy auto!");
+            } else {
+                System.out.println("Foglalt a Kocsimoso!");
+            }
         } else {
-            System.out.println("Foglalt a Kocsimoso!");
+            System.out.println("A kocsimoso elromlott! Nem tudd járművet fogadni!");
         }
     }
 
     public void normalMosas() {
-        jarmu[0].setTiszta(true);
-        jarmu[0].normalMosas();
+        if (tonkrement == false) {
+            jarmu[0].setTiszta(true);
+            jarmu[0].normalMosas();
+        } else {
+            System.out.println("A kocsimoso elromlott! Nem tudd mosni!");
+        }
     }
 
     public void gyengebbMosas() {
-        jarmu[0].setTiszta(true);
-        jarmu[0].gyengebbMosas();
+        if (tonkrement == false) {
+            jarmu[0].setTiszta(true);
+            jarmu[0].gyengebbMosas();
+        } else {
+            System.out.println("A kocsimoso elromlott! Nem tudd mosni!");
+        }
     }
 
     public void kimegy() {
@@ -38,7 +56,7 @@ public class Kocsimoso {
         if (jarmu[0] != null) {
             return jarmu[0].toString();
         } else {
-            return "üres a kocsimoso!";
+            return "Üres a kocsimoso!";
         }
     }
 
